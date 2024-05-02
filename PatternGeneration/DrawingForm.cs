ea145f82b6d179e7c9cd1cc51f810926dc8043da
Zedxel <ThomasTemby@hotmail.com>
2024-05-02 21:12:35 +1000

    Initial Project

---
+++ PatternGeneration/DrawingForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PatternGeneration
+{
+    public class DrawingForm : Form
+    {
+        public DrawingForm() 
+        {
+            BackColor = Color.AliceBlue;
+        }
+
+        private void DrawingForm(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PatternGeneration/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Runtime.Versioning;
+using System.Windows.Forms;
+
+namespace PatternGeneration
+{
+    class Program
+    {
+        [SupportedOSPlatform("windows6.1")]
+        public static void Main(string[] args)
+        {
+            Console.Title = "Fractal Generation";
+            DrawingForm MainApp = new();
+
+            Application.Run(MainApp);
+            MainApp.BackColor = Color.Red;
+            Application.DoEvents();
+        }
+
+    }
+}
